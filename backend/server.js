407b8b48ff59595c7d0d2e3b89a486eba9289d84
Umut-Polat-GM <umut.polat.272@gmail.com>
2025-03-11 16:22:24 +0300

    initialize backend structure with authentication, database connection, and user model

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.routes.js";
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server is running on port http://localhost:" + PORT);
+    connectDB();
+});
