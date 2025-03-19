a2da872434c4d1f467266e1edcfcce393807b0d4
Umut-Polat-GM <umut.polat.272@gmail.com>
2025-03-19 15:38:03 +0300

    add connection management functionality with request handling and notifications

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
 import notificationRoutes from "./routes/notification.routes.js";
+import connectionRoutes from "./routes/connection.routes.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -28,6 +29,7 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/notifications", notificationRoutes);
+app.use("/api/v1/connections", connectionRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port http://localhost:" + PORT);
